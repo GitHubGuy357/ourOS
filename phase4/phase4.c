a285bf00361de2923f742a0fa9c42751e407d88a
GitHubGuy357 <jarogers1@email.arizona.edu>
2017-10-23 20:47:02 -0700

    Pass 0: Ran test00, no system call takes place

--- phase4/phase4.c
+++ phase4/phase4.c
@@ -8,13 +8,15 @@
 #include "providedPrototypes.h"
 #include <stdlib.h> /* needed for atoi() */
 #include <stdio.h>
-//void (*systemCallVec[MAXSYSCALLS])(systemArgs *args);
+void (*systemCallVec[MAXSYSCALLS])(systemArgs *args);
 int 	mainSemaphore;
-
+int     debugVal = 1;
 static int	ClockDriver(char *);
 static int	DiskDriver(char *);
+static int	TermDriver(char *);
 
 void start3(void){
+	pDebug(1," <- start3(): start\n");
     char	name[128];
     char    termbuf[10];
     int		i;
@@ -78,6 +80,7 @@ void start3(void){
      */
     pid = spawnReal("start4", start4, NULL, 4 * USLOSS_MIN_STACK, 3);
     pid = waitReal(&status);
+	pDebug(1," <- start3(): after start4() \n");
 
     /*
      * Zap the device drivers
@@ -89,6 +92,263 @@ void start3(void){
     
 }
 
+
+/*************************************************************************
+ *
+ *                           Phase 4 Entry Point
+ *
+ ************************************************************************/
+//int start4(char * name){
+//	return -1;
+//}
+
+
+/*************************************************************************
+ *
+ *                           System Calls
+ *
+ ************************************************************************/
+
+/*****************************************************************************
+ *      Initialize systemCallVec calls
+ *****************************************************************************/
+ void intializeSysCalls(){
+	 
+	// Start by initially setting all the syscall vecs to a nullsys4
+    for (int i = 0; i < MAXSYSCALLS; i++) {
+        systemCallVec[i] = nullsys4;
+    }
+	
+	// set the appropriate system call handlers to their place in the system vector
+	// that way the appropriate error handler is called when an interrupt occurs
+	systemCallVec[SYS_SLEEP] = sleep;
+	systemCallVec[SYS_DISKREAD] = diskRead;
+	systemCallVec[SYS_DISKWRITE] = diskWrite;
+	systemCallVec[SYS_DISKSIZE] = diskSize;
+	systemCallVec[SYS_TERMREAD] = termRead;
+	systemCallVec[SYS_TERMWRITE] = termWrite;
+
+ } 
+ 
+/******************************************************************************
+ *  Routine: Sleep
+ *
+ *  Description: This is the call entry point for timed delay.
+ *  Arguments:    int seconds -- number of seconds to sleep
+ *  Return Value: 0 means success, -1 means error occurs
+ *
+ *****************************************************************************/
+int Sleep(int seconds){
+    USLOSS_Sysargs sysArg;
+    
+    CHECKMODE;
+    sysArg.number = SYS_SLEEP;
+    sysArg.arg1 = (void *)(long) seconds;
+    USLOSS_Syscall((void *)(long) &sysArg);
+    return (long) sysArg.arg4;
+} /* end of Sleep */
+
+int DiskRead(void *dbuff, int track, int first, int sectors, int unit, int *status){
+/******************************************************************************
+ *  Routine:  DiskRead
+ *  Description: This is the call entry point for disk input.
+ *  Arguments:    void* dbuff  -- pointer to the input buffer
+ *                int   track  -- first track to read 
+ *                int   first -- first sector to read
+ *		  int	sectors -- number of sectors to read
+ *		  int   unit   -- unit number of the disk
+ *                int   *status    -- pointer to output value
+ *                (output value: completion status)
+ *  Return Value: 0 means success, -1 means error occurs
+ *
+******************************************************************************/
+    USLOSS_Sysargs sysArg;
+    
+    CHECKMODE;
+    sysArg.number = SYS_DISKREAD;
+    sysArg.arg1 = dbuff;
+    sysArg.arg2 = (void *)(long) sectors;
+    sysArg.arg3 = (void *)(long) track;
+    sysArg.arg4 = (void *)(long) first;
+    sysArg.arg5 = (void *)(long) unit;
+    USLOSS_Syscall((void *)(long) &sysArg);
+    *status = (long) sysArg.arg1;
+    return (long) sysArg.arg4;
+} /* end of DiskRead */
+
+
+/******************************************************************************
+ *  Routine:  Sys_DiskWrite
+ *  Description: This is the call entry point for disk output.
+ *  Arguments:    void* dbuff  -- pointer to the output buffer
+ *                int   track  -- first track to write
+ *                int   first -- first sector to write
+ *		  int	sectors -- number of sectors to write
+ *		  int   unit   -- unit number of the disk
+ *                int   *status    -- pointer to output value
+ *                (output value: completion status)
+ *  Return Value: 0 means success, -1 means error occurs
+******************************************************************************/
+int DiskWrite(void *dbuff, int track, int first, int sectors, int unit, int *status){
+    USLOSS_Sysargs sysArg;
+
+    CHECKMODE;
+    sysArg.number = SYS_DISKWRITE;
+    sysArg.arg1 = dbuff;
+    sysArg.arg2 = (void *)(long) sectors;
+    sysArg.arg3 = (void *)(long) track;
+    sysArg.arg4 = (void *)(long) first;
+    sysArg.arg5 = (void *)(long) unit;
+    USLOSS_Syscall((void *) &sysArg);
+    *status = (long) sysArg.arg1;
+    return (long) sysArg.arg4;
+} /* end of DiskWrite */
+
+/******************************************************************************
+ *  Routine:  Sys_DiskSize
+ *
+ *  Description: Return information about the size of the disk.
+ *
+ *  Arguments:    int   unit  -- the unit number of the disk 
+ *                int   *sector -- bytes in a sector
+ *		  int	*track -- number of sectors in a track
+ *                int   *disk  -- number of tracks in the disk
+ *                (output value: completion status)
+ *
+ *  Return Value: 0 means success, -1 means invalid parameter
+ *****************************************************************************/
+int DiskSize(int unit, int *sector, int *track, int *disk){
+	USLOSS_Sysargs sysArg;
+
+	CHECKMODE;
+	sysArg.number = SYS_DISKSIZE;
+	sysArg.arg1 = (void *)(long) unit;
+	USLOSS_Syscall((void *)(long) &sysArg);
+	*sector = (long) sysArg.arg1;
+	*track = (long) sysArg.arg2;
+	*disk = (long) sysArg.arg3;
+	return (long) sysArg.arg4;
+} /* end of DiskSize */
+
+/******************************************************************************
+ *  Routine:  TermRead
+ *
+ *  Description: This is the call entry point for terminal input.
+ *
+ *  Arguments:    char *buff    -- pointer to the input buffer
+ *                int   bsize   -- maximum size of the buffer
+ *                int   unit_id -- terminal unit number
+ *                int  *nread      -- pointer to output value
+ *                (output value: number of characters actually read)
+ *
+ *  Return Value: 0 means success, -1 means error occurs
+ *****************************************************************************/
+int TermRead(char *buff, int bsize, int unit_id, int *nread){
+    USLOSS_Sysargs sysArg;
+
+    CHECKMODE;
+    sysArg.number = SYS_TERMREAD;
+    sysArg.arg1 = (void *)(long) buff;
+    sysArg.arg2 = (void *)(long) bsize;
+    sysArg.arg3 = (void *)(long) unit_id;
+    USLOSS_Syscall((void *)(long) &sysArg);
+    *nread = (long) sysArg.arg2;
+    return (long) sysArg.arg4;
+} 
+
+/******************************************************************************
+ *  Routine:  TermWrite
+ *
+ *  Description: This is the call entry point for terminal output.
+ *
+ *  Arguments:    char *buff    -- pointer to the output buffer
+ *                int   bsize   -- number of characters to write
+ *                int   unit_id -- terminal unit number
+ *                int  *nwrite      -- pointer to output value
+ *                (output value: number of characters actually written)
+ *
+ *  Return Value: 0 means success, -1 means error occurs
+ *****************************************************************************/
+int TermWrite(char *buff, int bsize, int unit_id, int *nwrite){
+    USLOSS_Sysargs sysArg;
+    
+    CHECKMODE;
+    sysArg.number = SYS_TERMWRITE;
+    sysArg.arg1 = (void *)(long) buff;
+    sysArg.arg2 = (void *)(long) bsize;
+    sysArg.arg3 = (void *)(long) unit_id;
+    USLOSS_Syscall((void *) &sysArg);
+    *nwrite = (long) sysArg.arg2;
+    return (long) sysArg.arg4;
+} 
+
+
+void sleep(systemArgs *args){
+	pDebug(1," <- sleep(): start \n");
+	
+}
+
+int sleepReal(){
+	return -1;
+}
+
+void diskRead(systemArgs *args){
+		pDebug(1," <- diskRead(): start \n");
+	
+}
+
+int diskReadReal(){
+	return -1;
+}
+
+void diskWrite(systemArgs *args){
+		pDebug(1," <- diskWrite(): start \n");
+}
+
+int diskWriteReal(){
+	return -1;
+}
+
+void diskSize(systemArgs *args){
+		pDebug(1," <- diskSize(): start \n");	
+}
+
+int diskSizeReal(){
+	return -1;
+}
+
+void termRead(systemArgs *args){
+		pDebug(1," <- diskSizeReal(): start \n");
+	
+}
+
+int termReadReal(){
+	return -1;
+}
+
+void termWrite(systemArgs *args){
+		pDebug(1," <- termWrite(): start \n");
+	
+}
+
+int termWriteReal(){
+	return -1;
+}
+
+/*************************************************************************
+*
+*                           DRIVERS
+*
+*************************************************************************/
+static int DiskDriver(char *arg)
+{
+    return 0;
+}
+
+static int TermDriver(char *arg)
+{
+    return 0;
+}
 
 static int ClockDriver(char *arg){
     int result;
@@ -112,56 +372,43 @@ static int ClockDriver(char *arg){
     }
 	return status;
 }
-
 /*************************************************************************
 *
-*                           Phase 4 Entry Point
+*                           UTILITIES
 *
 *************************************************************************/
-//int start4(char * name){
-//	return -1;
-//}
 
 
-/*************************************************************************
-*
-*                           System Calls
-*
-*************************************************************************/
-int Sleep(int seconds){
-	return -1;
+ /*****************************************************************************
+ *      nullsys4 - Initialize nullsys system call handler calls
+ *		Input:
+ *			Array of arguments
+ *****************************************************************************/
+void nullsys4(systemArgs *args) {
+    USLOSS_Console("nullsys4(): Invalid syscall %d. Halting...\n", args->number);
+	// Terminate instead of USLOSS_Halt(1) from phase1
+	terminateReal(getpid());
+} /* nullsys3 */
+
+/*****************************************************************************
+ *     putUserMode - puts the OS into usermode with the appropriate call to 
+ *		psrSet
+ *****************************************************************************/
+void putUserMode(){
+	int result = USLOSS_PsrSet(USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE);
+	pDebug(3," <- putUserMode(): Result = %d\n",result);
 }
 
-int DiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status){
-	return -1;
-}
-
-int DiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status){
-	return -1;
-}
-
-int DiskSize (int unit, int *sector, int *track, int *disk){
-	return -1;
-}
-
-int TermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead){
-	return -1;
-}
-
-int TermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead){
-	return -1;
-}
-
-
-/*************************************************************************
-*
-*                           DRIVERS
-*
-*************************************************************************/
-static int DiskDriver(char *arg)
-{
-    return 0;
-}
+/*****************************************************************************
+ *     pDebug - Outputs a printf-style formatted string to stdout
+ *****************************************************************************/
+int pDebug(int level, char *fmt, ...) {
+	 va_list args;
+	 va_start(args, fmt);
+     if(debugVal >= level)
+        USLOSS_VConsole(fmt,args);
+	 return 1;
+} /* pDebug */
 
 /*************************************************************************
 *   check_kernel_mode - Checks if the PsrBit is set to kernal mode
--- phase4/phase4.h
+++ phase4/phase4.h
@@ -5,12 +5,23 @@
 #ifndef _PHASE4_H
 #define _PHASE4_H
 #include "MinQueue.h"
+#include "phase1.h"
+#include "phase2.h"
+#include "phase3.h"
+#include "phase4.h"
+#include "providedPrototypes.h"
 /*
  * Maximum line length
  */
 
 #define MAXLINE         80
-
+#define CHECKMODE {    \
+    if (USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) { \
+        USLOSS_Console("Trying to invoke syscall from kernel\n"); \
+        USLOSS_Halt(1);  \
+    }  \
+}
+extern void (*systemCallVec[MAXSYSCALLS])(systemArgs *args); // Added to compile with WRONG systemArgs, RIGHT? cant use USLOSS
 /*
  * Function prototypes for this phase.
  */
@@ -21,11 +32,29 @@ extern  int  DiskWrite(void *diskBuffer, int unit, int track, int first, int sec
 extern  int  DiskSize (int unit, int *sector, int *track, int *disk);
 extern  int  TermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead);
 extern  int  TermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead);
-extern  int  start4(char *);
+extern  int  start4(char * name);
 
 // More prototypes
 int check_kernel_mode(char *procName);
+void putUserMode();
+int pDebug(int level, char *fmt, ...);
+void nullsys4(systemArgs *args);
 
+// Sys call prototypes
+void sleep(systemArgs *args);
+int sleepReal();
+void diskRead(systemArgs *args);
+int diskReadReal();
+void diskWrite(systemArgs *args);
+int diskWriteReal();
+void diskSize(systemArgs *args);
+int diskSizeReal();
+void termRead(systemArgs *args);
+int termReadReal();
+void termWrite(systemArgs *args);
+int termWriteReal();
+
+// Structures
 typedef struct procTable *procPtr;
 typedef struct procTable {
     int pid;
