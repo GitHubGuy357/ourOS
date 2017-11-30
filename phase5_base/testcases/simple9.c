c26e093ddeb0801dd3f1b26b7f5b06c0f60e4cd1
GitHubGuy357 <jarogers1@email.arizona.edu>
2017-11-30 06:02:21 -0700

    Phase 5 base new changes

--- phase5_base/testcases/simple9.c
+++ phase5_base/testcases/simple9.c
@@ -1,10 +1,9 @@
 /*
- * simple.c
+ * simple9.c
  *
- * One process, Writing and Reading the same data from the page with
- * a context switch in between.
- * No disk I/O should occur.  0 replaced pages and 1 page 
- * faults.  Check determanistic statistics.
+ * Two processes.  2 pages, 2 frames
+ * Each writing and reading data from page 0
+ * No disk I/O should occur.  0 replaced pages and 2 page faults
  */
 
 #include <usloss.h>
@@ -16,10 +15,10 @@
 
 #define Tconsole USLOSS_Console
 
-#define TEST        "simple1"
-#define PAGES       1
-#define CHILDREN    1
-#define FRAMES      1
+#define TEST        "simple9"
+#define PAGES       2
+#define CHILDREN    2
+#define FRAMES      2
 #define PRIORITY    5
 #define ITERATIONS  1
 #define PAGERS      1
@@ -37,15 +36,18 @@ void test_cleanup(int argc, char *argv[])
 {
 }
 
-
 int
 Child(char *arg)
 {
     int    pid;
-    char   str[64]= "This is the first page";
+    char   str[64];
 
     GetPID(&pid);
     Tconsole("\nChild(%d): starting\n", pid);
+
+             //             1         2          3         4
+             //   012345678901234567890123456787901234567890
+    sprintf(str, "This is the first page for pid %d\n", pid);
 
     Tconsole("Child(%d): str = %s\n", pid, str);
     Tconsole("Child(%d): strlen(str) = %d\n", pid, strlen(str));
@@ -55,29 +57,22 @@ Child(char *arg)
     Tconsole("Child(%d): after memcpy\n", pid);
 
     if (strcmp(vmRegion, str) == 0)
-        Tconsole("Child(%d): strcmp first attempt worked!\n", pid);
+        Tconsole("Child(%d): strcmp first attempt to read worked!\n", pid);
     else
-        Tconsole("Child(%d): Wrong string read, first attempt\n", pid);
+        Tconsole("Child(%d): Wrong string read, first attempt to read\n", pid);
 
-    assert(vmStats.faults == 1);
-    assert(vmStats.new == 1);
-
-    SemV(sem);  // forces a context switch with start5
+    SemV(sem); // to force context switch
 
     if (strcmp(vmRegion, str) == 0)
-        Tconsole("Child(%d): strcmp second attempt worked!\n", pid);
+        Tconsole("Child(%d): strcmp second attempt to read worked!\n", pid);
     else
-        Tconsole("Child(%d): Wrong string read, second attempt\n", pid);
+        Tconsole("Child(%d): Wrong string read, second attempt to read\n", pid);
 
     Tconsole("Child(%d): checking various vmStats\n", pid);
-    assert(vmStats.faults == 1);
-    assert(vmStats.new == 1);
-    assert(vmStats.pageOuts == 0);
-    assert(vmStats.pageIns == 0);
 
     Tconsole("Child(%d): terminating\n\n", pid);
 
-    Terminate(117);
+    Terminate(137);
     return 0;
 } /* Child */
 
@@ -85,8 +80,9 @@ Child(char *arg)
 int
 start5(char *arg)
 {
-    int  pid;
+    int  pid[CHILDREN];
     int  status;
+    char childName[20], letter;
 
     Tconsole("start5(): Running:    %s\n", TEST);
     Tconsole("start5(): Pagers:     %d\n", PAGERS);
@@ -104,15 +100,29 @@ start5(char *arg)
 
     SemCreate(0, &sem);
 
-    Spawn("Child", Child, NULL, USLOSS_MIN_STACK * 7, PRIORITY, &pid);
+    letter = 'A';
+    for (int i = 0; i < CHILDREN; i++) {
+        sprintf(childName, "Child%c", letter++);
+        Spawn(childName, Child, NULL, USLOSS_MIN_STACK*7, PRIORITY, &pid[i]);
+    }
 
-    SemP( sem);
+    // One P operation per child
+    // Both children are low priority, so we want the parent to wait until
+    // each child gets past their first strcpy into the VM region.
+    for (int i = 0; i < CHILDREN; i++)
+        SemP( sem);
 
-    Wait(&pid, &status);
-    assert(status == 117);
+    for (int i = 0; i < CHILDREN; i++) {
+        Wait(&pid[i], &status);
+        assert(status == 137);
+    }
+
+    assert(vmStats.faults == 2);
+    assert(vmStats.new == 2);
+    assert(vmStats.pageOuts == 0);
+    assert(vmStats.pageIns == 0);
 
     Tconsole("start5(): done\n");
-    //PrintStats();
     VmDestroy();
     Terminate(1);
 
